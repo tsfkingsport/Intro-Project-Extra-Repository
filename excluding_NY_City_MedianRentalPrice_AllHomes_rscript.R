5679b8fffbeee71158d004ae313fc4b56ac7dd8b
Tsfkingsport <tsfkingsport@hotmail.com>
2019-02-18 07:16:40 -0500

    Trying lm, getting errors

--- excluding_NY_City_MedianRentalPrice_AllHomes_rscript.R
+++ excluding_NY_City_MedianRentalPrice_AllHomes_rscript.R
@@ -25,7 +25,9 @@ names(rent_tibble) <-gsub("X","", names(rent_tibble))
 #of column names work and I don't know what is so special about those columns
 #as opposed to the rest of the columns. THey all appear to be the same format to me
 names(rent_tibble) <-gsub("\\.","-", names(rent_tibble))
-ymd(names(rent_tibble), truncated = 3)
+
+#The next line breaks my graphs and the whole script for some reason, Trying something else now
+#rent_tibble <- ymd(names(rent_tibble), truncated = 3)
 
 #Now going to try gathering the data so that its more tidy using date as the key
 #and rent as the value
@@ -35,19 +37,51 @@ Z<- gather(Z, key = "date", value = "rent", - 1)
 #I clean everything up. 
 rent_df<-Z[order(Z$RegionName),]
 
+rent_df$date <- ymd(rent_df$date, truncated = 2)
+
+
 rent_df %>% ggplot(aes(x = date, y = rent, colour = RegionName)) +
   geom_point()
 
 write.csv(rent_df, "excluding_NYC_MedianRentalPrice.csv")
-
+#Graphing the first few cities on their own
 rent_df[c(1:540),] %>% ggplot(aes(x = date, y = rent, colour = RegionName)) +
   geom_point()
 
+#Decided that using the excel sheet to find out when the cities ended in the csv
+#was simpler then trying to use which and graphing 2 cities at a time. 
+rent_df[c(541:1080),] %>% ggplot(aes(x = date, y = rent, colour = RegionName)) +
+  geom_point()
+
+rent_df[c(1081:1512),] %>% ggplot(aes(x = date, y = rent, colour = RegionName)) +
+  geom_point()
+
+rent_df[c(1513:1944),] %>% ggplot(aes(x = date, y = rent, colour = RegionName)) +
+  geom_point()
+#This does not work, Need to research further. Something about using multiple | for or statments.  
+#I can use one of them to graph 2 cities but more then that it breaks down and I am not sure why
+#I know that there is a difference between | and || but I am not sure what
+#that difference is or what it means in practical terms. 
+
+#rent_df[which(rent_df$RegionName =="Denver" || 
+     #           rent_df$RegionName =="Detroit") || 
+    #      rent_df$RegionName =="Durham"||
+   #     rent_df$RegionName =="Houston"] %>% 
+#  ggplot(aes(x = date, y = rent, colour = RegionName)) +
+#  geom_point()
+
+str(rent_df$RegionName)
+#Making linear models, starting with Atlanta
+Atlanta_lm <- lm(formula = rent ~ date, rent_df, subset = rent_df =="Atlanta",
+                 na.action = na.exclude)
+
+rent_df[which(rent_df$RegionName =="Atlanta"),] %>% 
+  ggplot(aes(x = date, y = rent, colour = RegionName)) +
+  geom_point()
 
 
-
-
-
+plot(Atlanta_lm) #The residuals are zero across the graph, that can't be right
+summary(Atlanta_lm)
 
 
 
